6b26817765fdec4d20027cd39446979b66f43149
Pedro Naponoceno <pnaponoceno@caiena.net>
2016-10-10 03:06:08 -0300

    Melhorando form de usuários

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,19 @@
+# customize registration controller
+class RegistrationsController < Devise::RegistrationsController
+  layout 'application'
+  skip_before_filter :require_no_authentication
+  before_filter :resource_name
+
+  def resource_name
+    :user
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    super
+  end
+end
