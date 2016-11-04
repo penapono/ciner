390725dfb102c2129e23446d7306c457e88c2370
Rubens Jr <grul@localhost.localdomain>
2016-11-04 21:08:11 -0200

    Correção

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,7 @@ class User < ActiveRecord::Base
 
   def age
     now = Time.now.utc.to_date
+    return 0 unless birthday
     now.year - birthday.year - (birthday.to_date.change(:year => now.year) > now ? 1 : 0)
   end
 
