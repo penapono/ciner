12b9bfb04ad17eac81661f354e56d6c0ba5b00d2
Rubens Jr <grul@localhost.localdomain>
2016-11-14 17:33:40 -0200

    melhorias rubens

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,25 @@
 //= require jquery_ujs
 //= require bootstrap.min
 //= require_tree .
+
+var timer = 0;
+var t0 = 0;
+var t1 = 0;
+
+$('#terms-link').click(function (event) {
+  t0 = Date.now();
+  showTerms();
+  event.preventDefault(); // Prevent link from following its href
+});
+
+function showTerms(){
+  $('.overlay-bg').show();
+  $('.terms-container').show();
+}
+
+function hideTerms(){
+  $('.overlay-bg').hide();
+  $('.terms-container').hide();
+  t1 = Date.now();
+  timer = timer + (t1-t0);
+}
