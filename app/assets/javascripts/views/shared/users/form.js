67a255c04df62be0746728b3545da6d03da1e187
Rubens Jr <grul@localhost.localdomain>
2016-11-21 13:29:35 -0200

    melhoria cadastro usr

--- app/assets/javascripts/views/shared/users/form.js
+++ app/assets/javascripts/views/shared/users/form.js
@@ -115,10 +115,46 @@ $('.overlay-bg').click(function (event) {
   event.preventDefault();
 });
 
+var CPFoptions =  {
+  onComplete: function() {
+    if(!validateCPF()) {
+      $('#user_cpf').val('');
+      alert('CPF inválido!');
+    }
+  },
+};
+
 $(document).ready(function(){
   $('#user_birthday').mask('00/00/0000');
-  $('#user_cpf').mask('000.000.000-00', {reverse: true});
+  $('#user_cpf').mask('000.000.000-00', CPFoptions);
   $('#user_phone').mask('(00) 0000-0000');
   $('#user_mobile').mask('(00) 0000-00000');
   $('#user_cep').mask('00000-000');
+});
+
+function validateCPF() {
+  strCPF = $('#user_cpf').cleanVal();
+  var Sum;
+  var Rest;
+  Sum = 0;
+  if (strCPF == "00000000000") return false;
+    
+  for (i=1; i<=9; i++) Sum = Sum + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+  Rest = (Sum * 10) % 11;
+  
+    if ((Rest == 10) || (Rest == 11))  Rest = 0;
+    if (Rest != parseInt(strCPF.substring(9, 10)) ) return false;
+  
+  Sum = 0;
+    for (i = 1; i <= 10; i++) Sum = Sum + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    Rest = (Sum * 10) % 11;
+  
+    if ((Rest == 10) || (Rest == 11))  Rest = 0;
+    if (Rest != parseInt(strCPF.substring(10, 11) ) ) return false;
+    return true;
+}
+
+$('div.col-xs-12.col-sm-3.pull-right input.btn').click(function (event) {
+  alert('ai');
+  event.preventDefault();
 });
