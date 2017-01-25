b31985078e0a07359218fa6447dcb45c5a9d1a9d
Pedro Naponoceno <pnaponoceno@caiena.net>
2017-01-25 02:54:48 -0200

    Melhorias

--- config/application.rb
+++ config/application.rb
@@ -8,18 +8,33 @@ Bundler.require(*Rails.groups)
 
 module Ciner
   class Application < Rails::Application
-    config.autoload_paths << Rails.root.join('lib')
-    config.active_record.default_timezone = :local
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    config.i18n.default_locale = "pt-BR"
-    config.time_zone = 'Brasilia'
-    config.encoding = "utf-8"
-    I18n.config.enforce_available_locales = false
-    config.generators.javascript_engine :js
-
-    config.active_record.raise_in_transactional_callbacks = true
-
+    # Carrega os arquivos de rotas separados em config/routes
     routes = Dir[Rails.root.join("config/routes/*.rb")] + config.paths['config/routes.rb']
     config.paths['config/routes.rb'] = routes
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    config.time_zone = 'Brasilia'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = "pt-BR"
+    config.encoding = "utf-8"
+    config.assets.enabled = true
+    config.assets.version = '1.0'
+
+    # Destivar wrap de campos com erros
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+      "#{html_tag}".html_safe
+    }
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
+    config.active_job.queue_adapter = :delayed_job
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
+    # This will route any exceptions caught to your router Rack app
+    config.exceptions_app = self.routes
   end
 end
