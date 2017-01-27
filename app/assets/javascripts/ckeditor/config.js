b6a6685dadc394b75529c412c6c2976a8d6eaeee
Pedro Naponoceno <pnaponoceno@caiena.net>
2017-01-27 04:01:03 -0200

    CKEditor Second

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,9 @@
+CKEDITOR.editorConfig = function (config) {
+  config.language = 'pt-BR';
+
+  config.toolbar_simple = [
+    ['Bold',  'Italic', '-', 'Link', 'Unlink'],
+  ];
+
+  config.toolbar = 'simple';
+};
