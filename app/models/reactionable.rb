0d27321c6a4bc02a6bbc8fecc62c0c107aa2bfc9
Pedro Naponoceno <pnaponoceno@caiena.net>
2017-01-30 15:22:53 -0200

    Correções

--- app/models/reactionable.rb
+++ app/models/reactionable.rb
@@ -13,12 +13,12 @@ module Reactionable
 
     def like_icon_class(user)
       return 'fa fa-thumbs-o-up' unless user
-      user.voted_up_on? self ? 'fa fa-thumbs-up' : 'fa fa-thumbs-o-up'
+      (user.voted_up_on? self) ? 'fa fa-thumbs-up' : 'fa fa-thumbs-o-up'
     end
 
     def dislike_icon_class(user)
       return 'fa fa-thumbs-o-down' unless user
-      user.voted_down_on? self ? 'fa fa-thumbs-down' : 'fa fa-thumbs-o-down'
+      (user.voted_down_on? self) ? 'fa fa-thumbs-down' : 'fa fa-thumbs-o-down'
     end
   end
 end
