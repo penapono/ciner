b8bbbddfe60c046f946103aafea257939fe50212
Pedro Naponoceno <pedro.gnaponoceno@gmail.com>
2017-04-03 08:44:12 -0300

    Crontab

--- lib/tasks/movie_sync.rake
+++ lib/tasks/movie_sync.rake
@@ -4,8 +4,9 @@ namespace :movie_sync do
   desc 'Make Ciner great Again!'
 
   task update: :environment do
-    Movie.where(lock_updates: false).first(25).each do |movie|
+    Movie.where(lock_updates: false).first(20).each do |movie|
       movie.api_transform
+      sleep 1
     end
   end
 end
--- lib/tasks/serie_sync.rb
+++ lib/tasks/serie_sync.rb
@@ -4,8 +4,9 @@ namespace :serie_sync do
   desc 'Make Ciner great Again!'
 
   task update: :environment do
-    Serie.where(lock_updates: false).first(25).each do |serie|
+    Serie.where(lock_updates: false).first(20).each do |serie|
       serie.api_transform
+      sleep 1
     end
   end
 end
