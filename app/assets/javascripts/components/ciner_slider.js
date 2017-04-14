c9fc957cf92186b39072b4f07c887d721566afaf
Pedro Naponoceno <pedro.gnaponoceno@gmail.com>
2017-04-14 13:15:11 -0300

    Melhorias no Slider

--- app/assets/javascripts/components/ciner_slider.js
+++ app/assets/javascripts/components/ciner_slider.js
@@ -7,119 +7,95 @@ var sliderCount = videoCount / showCount;
 var controlsWidth = 40;
 var scollWidth = 0;
 
-
 $(document).ready(function(){
     init();
-
 });
-$( window ).resize(function() {
+
+$(window).resize(function() {
     init();
 });
+
 function init(){
-    // elements
-    var win = $(window);
-    var sliderFrame = $(".slider-frame");
-    var sliderContainer = $(".slider-container");
-    var slide = $(".slide");
+  // elements
+  var win = $(window);
+  var sliderFrame = $(".slider-frame");
+  var sliderContainer = $(".slider-container");
+  var slide = $(".slide");
 
-    //counts
-    var scollWidth = 0;
+  //counts
+  var scollWidth = 0;
 
+  //sizes
+  var windowWidth = win.width();
+  var frameWidth = win.width() - 80;
 
-    //sizes
-    var windowWidth = win.width();
-    var frameWidth = win.width() - 80;
-     if(windowWidth >= 0 && windowWidth <= 414){
-       showCount = 2;
-   }else if(windowWidth >= 414 &&  windowWidth <= 768){
-       showCount = 3;
-   }else{
-       showCount = 4;
-   }
-    var videoWidth = ((windowWidth - controlsWidth * 2) / showCount );
-    var videoHeight = Math.round(videoWidth / (16/9));
+  if(windowWidth >= 0 && windowWidth <= 414){
+    showCount = 2;
+  } else if(windowWidth >= 414 &&  windowWidth <= 768) {
+    showCount = 3;
+  } else {
+    showCount = 4;
+  }
 
-    var videoWidthDiff = (videoWidth * scaling) - videoWidth;
-    var videoHeightDiff = (videoHeight * scaling) - videoHeight;
+  var videoWidth = ((windowWidth - controlsWidth * 2) / showCount );
+  var videoHeight = Math.round(videoWidth / (16/9));
 
-    //set sizes
-    // sliderFrame.width(windowWidth);
-    sliderFrame.width("100%");
-    // sliderFrame.height(videoHeight * scaling);
-    sliderFrame.height("195px");
+  var videoWidthDiff = (videoWidth * scaling) - videoWidth;
+  var videoHeightDiff = (videoHeight * scaling) - videoHeight;
 
-    sliderFrame.css("top", (videoHeightDiff / 2));
+  sliderFrame.width("100%");
+  sliderFrame.height("195px");
+  sliderFrame.css("top", (videoHeightDiff / 2));
 
-    sliderContainer.height(videoHeight * scaling);
-    sliderContainer.width((videoWidth * videoCount) + videoWidthDiff);
-    sliderContainer.css("top", (videoHeightDiff / 2));
-    sliderContainer.css("margin-left", (controlsWidth));
+  frameWidth = 520;
 
-    // slide.height(videoHeight);
-    slide.height("195px");
-    // slide.width(videoWidth);
-    slide.width("130px");
+  sliderContainer.height(videoHeight * scaling);
+  sliderContainer.width((videoWidth * videoCount) + videoWidthDiff);
+  sliderContainer.css("top", (videoHeightDiff / 2));
+  sliderContainer.css("margin-left", (controlsWidth));
 
-    //hover effect
-    $(".slide").mouseover(function() {
-        // $(this).css("width", videoWidth * scaling);
-        // $(this).css("height", videoHeight * scaling);
-        $(this).css("width", "140px");
-        $(this).css("top", -(videoHeightDiff / 2));
-        if($(".slide").index($(this)) == 0 || ($(".slide").index($(this))) % 4 == 0){
-          // do nothing
-        }
-        else if(($(".slide").index($(this)) + 1) % 4 == 0 && $(".slide").index($(this)) != 0){
-            $(this).parent().css("margin-left", -(videoWidthDiff - controlsWidth));
-        }
-        else{
-            $(this).parent().css("margin-left", - (videoWidthDiff / 2));
-        }
-    }).mouseout(function() {
-        $(this).css("width", "130px");
-        // $(this).css("height", videoHeight * 1);
-        $(this).css("top", 0);
-        $(this).parent().css("margin-left", controlsWidth);
-    });
+  slide.height("195px");
+  slide.width("130px");
 
-    // controls
-    controls(frameWidth, scollWidth);
+  // controls
+  controls(frameWidth, scollWidth);
 }
+
 function controls(frameWidth, scollWidth){
-    var prev = $(".prev");
-    var next = $(".next");
+  var prev = $(".prev");
+  var next = $(".next");
 
-    next.on("click", function(){
-        console.log(currentSliderCount);
-        console.log(sliderCount);
-        scollWidth = scollWidth + frameWidth;
-        var self = $(this),
-            sliderFrame = self.closest('.slider-frame'),
-            sliderContainer = sliderFrame.find('.slider-container');
+  next.on("click", function(){
+    scollWidth = scollWidth + frameWidth;
 
-        sliderContainer.animate({
-            left: - scollWidth
-        }, 300, function(){
-            if(currentSliderCount >= sliderCount-1){
-                sliderContainer.css("left", 0);
-                currentSliderCount = 0;
-                scollWidth = 0;
-            }else{
-                currentSliderCount++;
-            }
-        });
+    var self = $(this),
+        sliderFrame = self.closest('.slider-frame'),
+        sliderContainer = sliderFrame.find('.slider-container');
+
+    sliderContainer.animate({
+      left: -scollWidth
+    }, 300, function(){
+      if(currentSliderCount >= sliderCount-1){
+        sliderContainer.css("left", 0);
+        currentSliderCount = 0;
+        scollWidth = 0;
+      }else{
+        currentSliderCount++;
+      }
     });
+  });
 
-    prev.on("click", function(){
-        scollWidth = scollWidth - frameWidth;
-        var self = $(this),
-            sliderFrame = self.closest('.slider-frame'),
-            sliderContainer = sliderFrame.find('.slider-container');
+  prev.on("click", function(){
+    scollWidth = scollWidth - frameWidth;
 
-        sliderContainer.animate({
-            left: + scollWidth
-        }, 300, function(){
-            currentSliderCount--;
-        });
+    var self = $(this),
+        sliderFrame = self.closest('.slider-frame'),
+        sliderContainer = sliderFrame.find('.slider-container');
+
+    sliderContainer.animate({
+      left: + scollWidth
+    }, 300, function(){
+      currentSliderCount--;
     });
+  });
 };
