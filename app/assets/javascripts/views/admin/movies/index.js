25c11ce3b6fb041b423ef7ce7e9cb010cba6898b
Pedro Naponoceno <pedro.gnaponoceno@gmail.com>
2017-04-20 01:50:39 -0300

    Bulk delete Check-all

--- app/assets/javascripts/views/admin/movies/index.js
+++ app/assets/javascripts/views/admin/movies/index.js
@@ -1,6 +1,16 @@
 //= require views/shared/index
 
 $(function(){
+  $(document).on('click', "[data-check-all] input[type='checkbox']", function(){
+    var self = $(this),
+        parent = self.closest('[data-bulk-delete]'),
+        checkboxes = parent.find("[data-selectable] input[type='checkbox']");
+
+    checkboxes.each(function(index) {
+      $(this).prop('checked', self.prop('checked'));
+    });
+  });
+
   $(document).on('click', '[data-bulk-delete] [data-trash]', function(){
     var self = $(this),
         parent = self.closest('[data-bulk-delete]'),
@@ -26,6 +36,8 @@ $(function(){
         checkboxes.each(function(index) {
           $(this).prop('checked', false)
         });
+
+        $("[data-check-all] input[type='checkbox']").prop('checked', false);
         location.reload();
       },
       error: function(data) {
