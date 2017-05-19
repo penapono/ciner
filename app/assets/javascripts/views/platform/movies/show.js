9a45e3c08361fd2de8f6d6ae273b45ca3e3c7861
pnaponoceno <pedro.gnaponoceno@gmail.com>
2017-05-19 17:32:56 -0300

    Melhorias

--- app/assets/javascripts/views/platform/movies/show.js
+++ app/assets/javascripts/views/platform/movies/show.js
@@ -36,11 +36,41 @@ $(function() {
   });
 
   $('.modal').on('show.bs.modal', function () {
+    var self = $(this);
+
     $('.datepicker').mask('99/99/9999');
     $('.money').mask('000.000.000.000.000,00', {reverse: true});
     $('select').select2({
       theme: "bootstrap",
       minimumResultsForSearch: 50
+    });
+
+    self.on('click', '[data-brother-locker] input[type="checkbox"]', function(){
+      var self  = $(this),
+          status = self.prop('checked'),
+          lockerParent = self.closest('[data-brother-locker]'),
+          lockerId = lockerParent.data('brother-locker'),
+          brothers = self.closest('form').find("[data-brother-locked='" + lockerId + "'] input");
+
+      if (status) {
+        brothers.prop('disabled', true);
+      }else{
+        brothers.prop('disabled', false);
+      }
+    });
+
+    self.on('click', '[data-brother-unlocker] input[type="checkbox"]', function(){
+      var self  = $(this),
+          status = self.prop('checked'),
+          unlockerParent = self.closest('[data-brother-unlocker]'),
+          unlockerId = unlockerParent.data('brother-unlocker'),
+          brothers = self.closest('form').find("[data-brother-unlocked='" + unlockerId + "'] input");
+
+      if (status) {
+        brothers.prop('disabled', false);
+      }else{
+        brothers.prop('disabled', true);
+      }
     });
   });
 });
