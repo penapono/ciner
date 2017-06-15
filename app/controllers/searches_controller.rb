98de710d8df8d405c74eca3d13911a8a3fba1773
pnaponoceno <pedro.gnaponoceno@gmail.com>
2017-06-15 14:34:12 -0300

    Melhorias na Busca

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class SearchesController < ApplicationController
-  expose(:movies) { Movie.search(nil, params.fetch(:search, '')).limit(10) }
-  expose(:series) { Serie.search(nil, params.fetch(:search, '')).limit(10) }
-  expose(:professionals) { Professional.search(nil, params.fetch(:search, '')).limit(10) }
-  expose(:curriculums) { Curriculum.search(nil, params.fetch(:search, '')).limit(10) }
-  expose(:broadcasts) { Broadcast.search(nil, params.fetch(:search, '')).limit(10) }
-  expose(:events) { Event.search(nil, params.fetch(:search, '')).limit(10) }
-  expose(:critics) { Critic.search(nil, params.fetch(:search, '')).limit(10) }
-  expose(:questions) { Question.search(nil, params.fetch(:search, '')).limit(10) }
-  expose(:ciner_videos) { CinerVideo.search(nil, params.fetch(:search, '')).limit(10) }
+  expose(:movies) { Movie.search(nil, params.fetch(:search, '')).limit(4) }
+  expose(:series) { Serie.search(nil, params.fetch(:search, '')).limit(4) }
+  expose(:professionals) { Professional.search(nil, params.fetch(:search, '')).limit(4) }
+  expose(:curriculums) { Curriculum.search(nil, params.fetch(:search, '')).limit(4) }
+  expose(:broadcasts) { Broadcast.search(nil, params.fetch(:search, '')).limit(4) }
+  expose(:events) { Event.search(nil, params.fetch(:search, '')).limit(4) }
+  expose(:critics) { Critic.search(nil, params.fetch(:search, '')).limit(4) }
+  expose(:questions) { Question.search(nil, params.fetch(:search, '')).limit(4) }
+  expose(:ciner_videos) { CinerVideo.search(nil, params.fetch(:search, '')).limit(4) }
 end
