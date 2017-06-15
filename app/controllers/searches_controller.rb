175db669765e1b3661444aa529e078dd6f5693cb
pnaponoceno <pedro.gnaponoceno@gmail.com>
2017-06-15 14:18:50 -0300

    Melhorias Busca

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,6 +3,8 @@
 class SearchesController < ApplicationController
   expose(:movies) { Movie.search(nil, params.fetch(:search, '')).limit(15) }
   expose(:series) { Serie.search(nil, params.fetch(:search, '')).limit(15) }
+  expose(:professionals) { Professional.search(nil, params.fetch(:search, '')).limit(15) }
+  expose(:curriculums) { Curriculum.search(nil, params.fetch(:search, '')).limit(15) }
   expose(:broadcasts) { Broadcast.search(nil, params.fetch(:search, '')).limit(15) }
   expose(:events) { Event.search(nil, params.fetch(:search, '')).limit(15) }
   expose(:critics) { Critic.search(nil, params.fetch(:search, '')).limit(15) }
