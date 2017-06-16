3bb093584f7dc2464efecb212f7669aaaa9dc3f2
pnaponoceno <pedro.gnaponoceno@gmail.com>
2017-06-16 17:32:37 -0300

    Melhorias Ciner Videos

--- app/assets/javascripts/views/shared/form.js
+++ app/assets/javascripts/views/shared/form.js
@@ -11,6 +11,44 @@
 $(function() {
   'use strict';
 
+  $('.datepicker').mask('99/99/9999');
+  $('.money').mask('000.000.000.000.000,00', {reverse: true});
+  $('.year').mask('0000');
+  $('select').select2({
+    theme: "bootstrap",
+    minimumResultsForSearch: 50
+  });
+
+  $('[data-remote-select]').select2(_remoteDataConfig());
+
+  function _remoteDataConfig() {
+    return {
+      language: 'pt-BR',
+      allowClear: false,
+      minimumInputLength: 3,
+      closeOnSelect: true,
+      ajax: _defaultAjaxParams()
+    }
+  }
+
+  function _defaultAjaxParams() {
+    return {
+      dataType: 'json',
+      delay: 150,
+      data: function (search) {
+        return {
+          search: search
+        };
+      },
+
+      processResults: function (data) {
+        return {
+          results: data
+        };
+      }
+    };
+  }
+
   $('form').on("change", "#user_state_id", function(event) {
     var self = $(this),
         uf  = self.val();
