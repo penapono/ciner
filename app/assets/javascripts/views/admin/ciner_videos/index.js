d992dceafcda0706acf901a794deeabdde238502
pnaponoceno <pedro.gnaponoceno@gmail.com>
2017-06-26 04:30:21 -0300

    Melhorias

--- app/assets/javascripts/views/admin/ciner_videos/index.js
+++ app/assets/javascripts/views/admin/ciner_videos/index.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('[data-change-status]').on('click', 'button', function() {
     var self = $(this),
         id = self.data('ciner-video-id'),
-        url = "/admin/ciner_videos/"+ id +"/change_status";
+        url = "/admin/ciner_videos/" + id + "/change_status";
 
     $.ajax({
       type: 'PUT',
