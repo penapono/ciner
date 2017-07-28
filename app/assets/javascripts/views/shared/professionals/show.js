1cccd66a929bd1aacc0ebc8c7634278efb3d2db6
Pedro Naponoceno <pnaponoceno@daitangroup.com>
2017-07-28 05:24:56 -0700

    Colocando todas as set functions disponíveis para profissionais na tela de profissional

--- app/assets/javascripts/views/shared/professionals/show.js
+++ app/assets/javascripts/views/shared/professionals/show.js
@@ -2,15 +2,19 @@
 //= require components/ciner_tipsy
 
 $(document).ready(function() {
-  var container = $('[data-slider-playing]');
-  _sliderize(container);
+  var containers = $('[data-slider-playing]');
+
+  for(var i = 0; i < containers.length; i++) {
+    _sliderize($(containers[i]));
+  }
+  // _sliderize(container);
   // $('.slider-container .slide:nth-last-child(-n+4)').prependTo('.slider-container');
 });
 
-$(window).resize(function() {
-  var container = $('[data-slider-playing]');
-  _sliderize(container);
-});
+// $(window).resize(function() {
+//   var container = $('[data-slider-playing]');
+//   _sliderize(container);
+// });
 
 function _sliderize(aContainer) {
   var cinerSlider = new CinerSlider();
