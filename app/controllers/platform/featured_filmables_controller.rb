b5cdb722c89cac61084ae341e08d98a719aed5fb
Pedro Naponoceno <pnaponoceno@daitangroup.com>
2017-08-29 12:13:27 -0700

    Filmes em Destaque

--- app/controllers/platform/featured_filmables_controller.rb
+++ app/controllers/platform/featured_filmables_controller.rb
@@ -5,20 +5,10 @@ module Platform
     include Platform::FeaturedFilmablesBreadcrumb
 
     # exposes
-    expose(:filmables) { Movie.featured.first(100) }
-
-    PER_PAGE = 50
+    expose(:filmables) { Movie.featured.first(10) }
 
     def index
       return if filmables.blank?
-    end
-
-    private
-
-    # Filtering
-
-    def paginated_filmables
-      filmables.page(params[:page]).per(PER_PAGE)
     end
   end
 end
