50cd88e147f6503c9530f065278f467187f5359e
pnaponoceno <pedro.gnaponoceno@gmail.com>
2017-10-19 01:54:22 -0200

    Melhorias nas capas

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 module ImageHelper
-  def image_default_path(image_path, image_model)
-    return image_path if File.exists? image_path
-    image_path("default/#{image_model}/image.jpg")
+  def image_default_path(image, image_model)
+    image_path = image.path
+    return image.url if image_path && (File.exists? image_path)
+    # image_path("/default/#{image_model}/image.jpg")
+    "assets/default/#{image_model}/image.jpg"
   end
 end
