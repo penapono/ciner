70436d51f5ca30923dd64d113a8b07964cd0ad08
pnaponoceno <pedro.gnaponoceno@gmail.com>
2017-11-26 21:16:01 -0200

    Melhorias

--- app/assets/javascripts/views/api/v1/comments/index.js
+++ app/assets/javascripts/views/api/v1/comments/index.js
@@ -29,6 +29,41 @@ $(function() {
     });
   }
 
+  function _bindLimitedInput() {
+    var textArea = $('[data-limited] textarea'),
+        input = $('[data-limited] input');
+
+    var arrayLength = textArea.length;
+    for (var i = 0; i < arrayLength; i++) {
+      _calculateRemaining($(textArea[i]));
+    }
+
+    arrayLength = input.length;
+    for (var i = 0; i < arrayLength; i++) {
+      _calculateRemaining($(input[i]));
+    }
+
+    $('[data-limited]').on('input', 'textarea', function() {
+      var input = $(this);
+      _calculateRemaining(input);
+    });
+
+    $('[data-limited]').on('input', 'input', function() {
+      var input = $(this);
+      _calculateRemaining(input);
+    });
+  }
+
+  function _calculateRemaining(aInput) {
+    var parent = aInput.closest('[data-limited]'),
+        typed = aInput.val().length,
+        limit = parent.data('limit'),
+        charCounter = parent.find('.char-counter'),
+        remaining = limit - typed;
+
+    charCounter.text(remaining +' caracteres restantes.');
+  }
+
   function _reloadContent(aContainer, aData) {
     aContainer.empty().append(aData);
 
@@ -52,6 +87,7 @@ $(function() {
         }
       });
     });
+    _bindLimitedInput();
   }
 
   function _deleteComment() {
