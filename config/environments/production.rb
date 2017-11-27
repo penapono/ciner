b45011c61505c32d34fc6488962c7c38c2cd380b
pnaponoceno <pedro.gnaponoceno@gmail.com>
2017-11-27 02:49:51 -0200

    Melhoria

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,12 +17,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   # SMTP settings for gmail
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => 'cinernaponoceno@gmail.com',
-    :password             => 'cinerciner2017',
-    :authentication       => :login,
-    :enable_starttls_auto => true
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :authentication => :plain,
+    :user_name => 'cinernaponoceno@gmail.com',
+    :password => 'cinerciner2017'
   }
 end
