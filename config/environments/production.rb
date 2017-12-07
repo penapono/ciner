8f7ded9bdf6f2637b05f87e1cc87d86e2b508952
pnaponoceno-daitan <pnaponoceno@daitangroup.com>
2017-12-07 14:43:24 -0200

    Mudança do E-mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
       :address => "smtp.gmail.com",
       :port => 587,
       :authentication => :plain,
-      :user_name => 'cinernaponoceno@gmail.com',
+      :user_name => 'sejaciner@gmail.com',
       :password => 'cinerciner2017'
   }
 
