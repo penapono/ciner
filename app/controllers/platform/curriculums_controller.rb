f2fcfb20e4be851230e019e71e55f6c30df141fe
pnaponoceno <pedro.gnaponoceno@gmail.com>
2017-12-16 11:22:04 -0200

    Correção

--- app/controllers/platform/curriculums_controller.rb
+++ app/controllers/platform/curriculums_controller.rb
@@ -2,122 +2,122 @@
 
 module Platform
   class CurriculumsController < PlatformController
-   include Platform::CurriculumsBreadcrumb
+    include Platform::CurriculumsBreadcrumb
 
-  # exposes
-   expose(:curriculums) { Curriculum.all }
-   expose(:curriculum, attributes: :curriculum_attributes)
-  PER_PAGE = 50
+    # exposes
+    expose(:curriculums) { Curriculum.all }
+    expose(:curriculum, attributes: :curriculum_attributes)
+    PER_PAGE = 50
 
-   def index
-     return if curriculums.blank?
-     self.curriculums = paginated_curriculums
-nd
+    def index
+      return if curriculums.blank?
+      self.curriculums = paginated_curriculums
+    end
 
-   def create
-     if created?
-       redirect_to_show_with_success
-     else
-       render_new_with_error
-     end
-   end
+    def create
+      if created?
+        redirect_to_show_with_success
+      else
+        render_new_with_error
+      end
+    end
 
-   def destroy
-     destroyed?
-     redirect_to url_for([:platform, "profile"])
-   end
+    def destroy
+      destroyed?
+      redirect_to url_for([:platform, "profile"])
+    end
 
     private
 
-   # Filtering
+    # Filtering
 
-   def paginated_curriculums
-     filtered_curriculum.page(params[:page]).per(PER_PAGE)
-   end
+    def paginated_curriculums
+      filtered_curriculum.page(params[:page]).per(PER_PAGE)
+    end
 
-   def filtered_curriculum
-     curriculums.filter_by(searched_curriculums, params.fetch(:filter, ''))
-   end
+    def filtered_curriculum
+      curriculums.filter_by(searched_curriculums, params.fetch(:filter, ''))
+    end
 
-   def searched_curriculums
-     curriculums.search(nil, params.fetch(:search, ''))
-   end
+    def searched_curriculums
+      curriculums.search(nil, params.fetch(:search, ''))
+    end
 
-   def resource
-     curriculum
-   end
+    def resource
+      curriculum
+    end
 
-   def resource_title
-     curriculum.user_name
-   end
+    def resource_title
+      curriculum.user_name
+    end
 
-   def index_path
-     platform_curriculums_path
-   end
+    def index_path
+      platform_curriculums_path
+    end
 
-   def show_path
-     platform_curriculum_path(resource)
-   end
+    def show_path
+      platform_curriculum_path(resource)
+    end
 
-   def resource_params
-     curriculum_params
-   end
+    def resource_params
+      curriculum_params
+    end
 
-   def curriculum_params
-     params.require(:curriculum).permit(
-       :play_name,
-       :avatar,
-       :biography,
+    def curriculum_params
+      params.require(:curriculum).permit(
+        :play_name,
+        :avatar,
+        :biography,
 
-       :curriculum_function_id,
+        :curriculum_function_id,
 
-       # If Professional is an User
-       :user,
+        # If Professional is an User
+        :user,
 
-       # Measures
-       :mannequin,
-       :height,
-       :ethnicity,
+        # Measures
+        :mannequin,
+        :height,
+        :ethnicity,
 
-       :drt,
-       curriculum_photos_attributes: %i[
-         media
-         curriculum_id
-         id
-         _destroy
-       ],
-       curriculum_audios_attributes: %i[
-         media
-         curriculum_id
-         id
-         _destroy
-       ],
-       curriculum_videos_attributes: %i[
-         media
-         curriculum_id
-         id
-         _destroy
-       ],
-       curriculum_files_attributes: %i[
-         media
-         curriculum_id
-         id
-         _destroy
-       ],
-       curriculum_jobs_attributes: %i[
-         description
-         curriculum_id
-         id
-         _destroy
-       ],
-       curriculum_awards_attributes: %i[
-         category
-         title
-         curriculum_id
-         id
-         _destroy
-       ]
-     )
-   end
+        :drt,
+        curriculum_photos_attributes: %i[
+          media
+          curriculum_id
+          id
+          _destroy
+        ],
+        curriculum_audios_attributes: %i[
+          media
+          curriculum_id
+          id
+          _destroy
+        ],
+        curriculum_videos_attributes: %i[
+          media
+          curriculum_id
+          id
+          _destroy
+        ],
+        curriculum_files_attributes: %i[
+          media
+          curriculum_id
+          id
+          _destroy
+        ],
+        curriculum_jobs_attributes: %i[
+          description
+          curriculum_id
+          id
+          _destroy
+        ],
+        curriculum_awards_attributes: %i[
+          category
+          title
+          curriculum_id
+          id
+          _destroy
+        ]
+      )
+    end
   end
 end
