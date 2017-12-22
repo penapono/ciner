12b483f658ae802d199f6b311259ea5f40f820f1
pnaponoceno <pedro.gnaponoceno@gmail.com>
2017-12-22 01:03:07 -0200

    melhoria

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -2,13 +2,13 @@
 
 module ImageHelper
   def image_default_path(image, image_model)
-    return "assets/default/#{image_model}/image.jpg" if image.blank?
+    return "default/#{image_model}/image.jpg" if image.blank?
     image_path = if image.class == String
                    image
                  else
                    image.path
                  end
     return image.url if image_path && (File.exist? image_path)
-    "assets/default/#{image_model}/image.jpg"
+    "default/#{image_model}/image.jpg"
   end
 end
