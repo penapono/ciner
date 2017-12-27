e75f139ea8501ab3b72f2b000ce9191eaa5c080d
pnaponoceno-daitan <pnaponoceno@daitangroup.com>
2017-12-27 14:09:54 -0200

    Removing email uniqueness

---
+++ db/migrate/20171227160716_remove_email_uniqueness_on_users.rb
@@ -0,0 +1,6 @@
+class RemoveEmailUniquenessOnUsers < ActiveRecord::Migration[5.1]
+  def change
+    remove_index :users, :email
+    add_index :users, :email
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171227135723) do
+ActiveRecord::Schema.define(version: 20171227160716) do
 
   create_table "age_ranges", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -689,7 +689,7 @@ ActiveRecord::Schema.define(version: 20171227135723) do
     t.index ["city_id"], name: "index_users_on_city_id"
     t.index ["country_id"], name: "index_users_on_country_id"
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "index_users_on_email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["state_id"], name: "index_users_on_state_id"
   end
