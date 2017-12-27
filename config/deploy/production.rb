7f44e0b0bcdb8c5bfdda59acef5379f196f900a0
pnaponoceno-daitan <pnaponoceno@daitangroup.com>
2017-12-27 12:40:05 -0200

    Voltando ao IP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{ciner@ciner.com.br}
-role :web, %w{ciner@ciner.com.br}
-role :db,  %w{ciner@ciner.com.br}
+role :app, %w{ciner@45.55.81.37}
+role :web, %w{ciner@45.55.81.37}
+role :db,  %w{ciner@45.55.81.37}
 
 
 # Extended Server Syntax
