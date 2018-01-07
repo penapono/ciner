6487129d89487ba5ed27d39273e43556fcc53718
pnaponoceno <pedro.gnaponoceno@gmail.com>
2018-01-07 21:59:02 -0200

    Melhorias

--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -32,13 +32,13 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   process :extract_dimensions
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process resize_to_fill: [118, 100]
-  end
+  # version :thumb do
+  #   process resize_to_fill: [118, 100]
+  # end
 
-  version :content do
-    process resize_to_limit: [800, 800]
-  end
+  # version :content do
+  #   process resize_to_limit: [800, 800]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
--- app/uploaders/curriculum_photo_uploader.rb
+++ app/uploaders/curriculum_photo_uploader.rb
@@ -30,14 +30,14 @@ class CurriculumPhotoUploader < CarrierWave::Uploader::Base
     process scale: [200, 300]
   end
 
-  version :avatar do
-    process resize_to_fill: [150, 150]
-  end
+  # version :avatar do
+  #   process resize_to_fill: [150, 150]
+  # end
 
-  # Create different versions of your uploaded files:
-  version :thumb do
-    process resize_to_fit: [50, 50]
-  end
+  # # Create different versions of your uploaded files:
+  # version :thumb do
+  #   process resize_to_fit: [50, 50]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
