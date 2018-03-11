d553e413e094a7c646d5fcc8e21da3b929c38e3a
pnaponoceno <pedro.gnaponoceno@gmail.com>
2018-03-10 22:34:54 -0300

    Colocar título na página de Notificações

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,7 +14,7 @@ end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
