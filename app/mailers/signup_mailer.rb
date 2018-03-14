93c278c69890e50c35413925e709ea2dfa08cce1
Pedro Naponoceno <pedro.gnaponoceno@gmail.com>
2018-03-14 12:10:28 +0000

    titulo

--- app/mailers/signup_mailer.rb
+++ app/mailers/signup_mailer.rb
@@ -8,6 +8,6 @@ class SignupMailer < ApplicationMailer
     attachments.inline["instagram.png"] = File.read("#{Rails.root}/app/assets/images/instagram2.png")
     attachments.inline["facebook.png"] = File.read("#{Rails.root}/app/assets/images/facebook.png")
     attachments.inline["logo.jpg"] = File.read("#{Rails.root}/app/assets/images/logo.jpg")
-    mail(to: email, subject: "Bem-vindo, CINER!", reply_to: 'ciner@gmail.com')
+    mail(to: email, subject: "Bem-vindo, ciner!", reply_to: 'ciner@gmail.com')
   end
 end
