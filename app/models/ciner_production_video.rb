04f41bc9a97ad5c5f754294cda90de68677bf55e
Pedro Naponoceno <pedro.gnaponoceno@gmail.com>
2018-04-11 20:27:53 +0000

    Melhoria

--- app/models/ciner_production_video.rb
+++ app/models/ciner_production_video.rb
@@ -11,9 +11,22 @@ class CinerProductionVideo < ActiveRecord::Base
   # Scope
   default_scope { order(season: :asc, episode: :asc) }
 
+  # Callbacks
+  before_save :update_links
+
   # Filter
 
   def self.filter_by(collection, _params)
     collection
+  end
+
+  # Callbacks
+  def update_links
+    video.gsub!('youtube.com/watch?v=', 'youtube.com/embed/')
+    video.gsub!('https://vimeo.com/', 'https://player.vimeo.com/video/')
+    video.gsub!('http://vimeo.com/', 'https://player.vimeo.com/video/')
+    video.gsub!('www.vimeo.com/', 'https://player.vimeo.com/video/')
+    video.gsub!('https://www.vimeo.com/', 'https://player.vimeo.com/video/')
+    video.gsub!('http://www.vimeo.com/', 'https://player.vimeo.com/video/')
   end
 end
