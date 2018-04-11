4f9d932ee2820f41bd307ca9a292812edf972b4d
Pedro Naponoceno <pedro.gnaponoceno@gmail.com>
2018-04-11 18:54:07 +0000

    Generalista

--- app/models/curriculum_function.rb
+++ app/models/curriculum_function.rb
@@ -13,6 +13,9 @@ class CurriculumFunction < ActiveRecord::Base
   # Scope
   default_scope { order(name: :asc) }
 
+  # Aliases
+  alias_attribute :text, :general
+
   # Filter
 
   def self.filter_by(collection, _params)
