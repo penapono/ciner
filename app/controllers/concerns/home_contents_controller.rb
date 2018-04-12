258884917387a2076884429b9baba33ddd8256d0
Pedro Naponoceno <pedro.gnaponoceno@gmail.com>
2018-04-12 18:50:15 +0000

    Correção

--- app/controllers/concerns/home_contents_controller.rb
+++ app/controllers/concerns/home_contents_controller.rb
@@ -6,13 +6,13 @@ module HomeContentsController
   included do
     # Exposes
     expose(:events) { Event.all_next.first(4) }
-    expose(:critics) { Critic.home }
+    expose(:critics) { Critic.where(quick: false).home }
     expose(:questions) { Question.includes(:user).top_questions }
     expose(:broadcasts) { Broadcast.top_broadcasts.first(4) }
     expose(:trending_trailers) { TrendingTrailer.includes(:filmable).first(3) }
 
     expose(:playing_movies) { Movie.current_playing.limit(20) }
     expose(:featured_movies) { Movie.featured(10) }
-    expose(:ciner_productions) { CinerProduction.approved.where(quick: false).first(4) }
+    expose(:ciner_productions) { CinerProduction.approved.first(4) }
   end
 end
