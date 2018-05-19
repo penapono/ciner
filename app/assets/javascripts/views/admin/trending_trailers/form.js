1a96407425ed896e504b1256cb8fb28b72cd4382
Pedro Naponoceno <pedro.gnaponoceno@gmail.com>
2018-05-19 16:51:54 -0300

    Melhorias

--- app/assets/javascripts/views/admin/trending_trailers/form.js
+++ app/assets/javascripts/views/admin/trending_trailers/form.js
@@ -33,4 +33,34 @@ $(function() {
     // Show only the desired select
     aSelectToRemoveClass.removeClass(aClass);
   }
+
+  $('[data-remote-select]').select2(_remoteDataConfig());
+
+    function _remoteDataConfig() {
+      return {
+        language: 'pt-BR',
+        allowClear: false,
+        minimumInputLength: 3,
+        closeOnSelect: true,
+        ajax: _defaultAjaxParams()
+      }
+    }
+
+    function _defaultAjaxParams() {
+      return {
+        dataType: 'json',
+        delay: 150,
+        data: function (search) {
+          return {
+            search: search
+          };
+        },
+
+        processResults: function (data) {
+          return {
+            results: data
+          };
+        }
+      };
+    }
 });
