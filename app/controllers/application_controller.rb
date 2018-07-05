af220da130c4bb59c865373cbb3d06a241bb17f7
Pedro Naponoceno <pedro.gnaponoceno@gmail.com>
2018-07-04 23:31:10 -0300

    Correções

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,7 @@ class ApplicationController < ActionController::Base
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery prepend: true, with: :exception
-  before_action :authenticate_user!
+  protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
