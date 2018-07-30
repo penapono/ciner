47393d27a97d02c95773289aac02174855593617
Pedro Naponoceno <pedro.gnaponoceno@gmail.com>
2018-07-29 21:10:09 -0300

    Melhorias header e footer'

--- app/assets/javascripts/components/header.js
+++ app/assets/javascripts/components/header.js
@@ -1,43 +1,43 @@
-$(function(){
-  $(window).scroll(function () {
-    var self = $(this);
+// $(function(){
+//   $(window).scroll(function () {
+//     var self = $(this);
 
-    _controlNav(self);
-  });
+//     _controlNav(self);
+//   });
 
-  function _controlNav(aWindow) {
-    var nav = $('.nav-container'),
-        containerFull = nav.find('[data-container-full]'),
-        containerFixed = nav.find('[data-container-fixed]');
+//   function _controlNav(aWindow) {
+//     var nav = $('.nav-container'),
+//         containerFull = nav.find('[data-container-full]'),
+//         containerFixed = nav.find('[data-container-fixed]');
 
-    if (aWindow.scrollTop() > 310) {
-      _lockNav(nav, containerFixed, containerFull);
-    } else {
-      _releaseNav(nav, containerFull, containerFixed)
-    }
-  }
+//     if (aWindow.scrollTop() > 310) {
+//       _lockNav(nav, containerFixed, containerFull);
+//     } else {
+//       _releaseNav(nav, containerFull, containerFixed)
+//     }
+//   }
 
-  function _lockNav(aNav, aContainerToShow, aContainerToHide) {
-    if (!aNav.hasClass('f-nav')) {
-      _manageNav(aNav, '', 'f-nav');
-      _manageContainers(aContainerToShow, aContainerToHide);
-    }
-  }
+//   function _lockNav(aNav, aContainerToShow, aContainerToHide) {
+//     if (!aNav.hasClass('f-nav')) {
+//       _manageNav(aNav, '', 'f-nav');
+//       _manageContainers(aContainerToShow, aContainerToHide);
+//     }
+//   }
 
-  function _releaseNav(aNav, aContainerToShow, aContainerToHide) {
-    if (aNav.hasClass('f-nav')) {
-      _manageNav(aNav, 'f-nav', '');
-      _manageContainers(aContainerToShow, aContainerToHide);
-    }
-  }
+//   function _releaseNav(aNav, aContainerToShow, aContainerToHide) {
+//     if (aNav.hasClass('f-nav')) {
+//       _manageNav(aNav, 'f-nav', '');
+//       _manageContainers(aContainerToShow, aContainerToHide);
+//     }
+//   }
 
-  function _manageContainers(aContainerToShow, aContainerToHide) {
-    aContainerToShow.removeClass("collapse");
-    aContainerToHide.addClass("collapse");
-  }
+//   function _manageContainers(aContainerToShow, aContainerToHide) {
+//     aContainerToShow.removeClass("collapse");
+//     aContainerToHide.addClass("collapse");
+//   }
 
-  function _manageNav(aNav, aClassToRemove, aClassToAdd) {
-    aNav.addClass(aClassToAdd);
-    aNav.removeClass(aClassToRemove);
-  }
-});
+//   function _manageNav(aNav, aClassToRemove, aClassToAdd) {
+//     aNav.addClass(aClassToAdd);
+//     aNav.removeClass(aClassToRemove);
+//   }
+// });
