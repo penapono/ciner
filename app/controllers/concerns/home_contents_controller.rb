dafe1e2375fc5faac154263419f2caceed079de0
Pedro Naponoceno <pedro.gnaponoceno@gmail.com>
2018-07-31 02:25:09 -0300

    Melhorias home

--- app/controllers/concerns/home_contents_controller.rb
+++ app/controllers/concerns/home_contents_controller.rb
@@ -6,12 +6,14 @@ module HomeContentsController
   included do
     # Exposes
     expose(:events) { Event.all_next.first(4) }
-    expose(:critics) { Critic.where(quick: false).home }
+    expose(:critics) { Critic.where(quick: false).ordered_by_status.first(6) }
     expose(:questions) { Question.includes(:user).top_questions }
     expose(:broadcasts) { Broadcast.top_broadcasts.first(4) }
     expose(:trending_trailers) { TrendingTrailer.includes(:filmable).first(3) }
 
     expose(:playing_movies) { Movie.current_playing.limit(10) }
+    expose(:playing_series) { Serie.current_playing.limit(10) }
+    expose(:playing_soon_filmables) { Movie.playing_soon.limit(10) }
     expose(:featured_movies) { Movie.featured(10) }
     expose(:ciner_productions) { CinerProduction.approved.includes(:user, :ciner_production_videos).first(4) }
   end
